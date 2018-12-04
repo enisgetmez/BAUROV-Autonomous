b4464fc2ab46af667d0d06be217f2b6512e55bb2
Enis Getmez <mentalistler@mit.tc>
2018-12-04 17:44:20 +0300

    Create justcolortracking.py

---
+++ Autonomous/justcolortracking.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from collections import deque
+import numpy as np
+import argparse
+import cv2
+import time
+import imutils
+
+cap = cv2.VideoCapture(0) # webcamin bagli oldugu port varsayilan 0 
+
+ret, frame = cap.read() # kameradan gelen görüntülerin alınması
+
+#sari rengin algilanmasi
+colorLower = (24, 100, 100) 
+colorUpper = (44, 255, 255)
+#converter.py ile convert ettiğiniz rengi buraya giriniz
+camera = cv2.VideoCapture(0) # kameradan 
+while True: #yazılımımız çalıştığı sürece aşağıdaki işlemleri tekrarla
+
+	
+     (grabbed, frame) = camera.read() # grabbed ve frame değişkenini camera.read olarak tanımlıyoruz.
+
+     frame = imutils.resize(frame, width=600) # görüntü genişliğini 600p yapıyoruz
+     frame = imutils.rotate(frame, angle=180) # görüntüyü 180 derece döndürüyoruz
+
+     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # görüntüyü hsv formatına çeviriyoruz
+ 
+	
+     mask = cv2.inRange(hsv, colorLower, colorUpper) # hsv formatına dönen görüntünün bizim belirttiğimiz renk sınırları içerisinde olanları belirliyor
+     mask = cv2.erode(mask, None, iterations=2) # bizim renklerimizi işaretliyor
+     mask = cv2.dilate(mask, None, iterations=2) # bizim renklerimizin genişliğini alıyor
+	
+	
+     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+	 cv2.CHAIN_APPROX_SIMPLE)[-2]
+     center = None
+ 
+
+     if len(cnts) > 0:
+
+		     c = max(cnts, key=cv2.contourArea)
+		     ((x, y), radius) = cv2.minEnclosingCircle(c)
+		     M = cv2.moments(c)
+		     center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+ 
+		
+		     if radius > 10: #algılanacak hedefin minumum boyutu
+			     cv2.circle(frame, (int(x), int(y)), int(radius),
+				 (0, 255, 255), 2)
+			     cv2.circle(frame, center, 5, (0, 0, 255), -1)
+     print("x : ")
+     print(x) # kameradan gelen görüntüde bizim rengimiz varsa x kordinatı
+     print("y : ")
+     print(y) # kameradan gelen görüntüde bizim rengimiz varsa y kordinatı
